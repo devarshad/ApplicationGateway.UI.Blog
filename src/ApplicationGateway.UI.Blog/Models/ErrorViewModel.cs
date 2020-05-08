1ae2e9ab95cd1fe6012a8eddd7470bd11925909e
mohkhan26 <mohammed.khan2@publicissapient.com>
2020-05-08 21:06:31 +0530

    Master commit

---
+++ src/ApplicationGateway.UI.Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApplicationGateway.UI.Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
